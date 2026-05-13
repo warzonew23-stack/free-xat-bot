6c23c07509f289d526155b88f2d2f9d8b2fa301b
warzonew23-stack <warzonew23@gmail.com>
2026-05-14 01:40:22 +0300

    Update Database.js

--- src/core/Database.js
+++ src/core/Database.js
@@ -1,7 +1,9 @@
 import { Sequelize } from "sequelize";
+import sqlite3 from "sqlite3"; // Ezzel rákényszerítjük a Vercelt, hogy csomagolja be!
 
 export const sequelize = new Sequelize({
     dialect: "sqlite",
-    storage: "./database.db",
+    dialectModule: sqlite3, // Megmondjuk neki, hogy használja a betöltött modult
+    storage: "/tmp/database.db", // A Vercelen csak a /tmp mappába van jogunk írni!
     logging: false
 });
